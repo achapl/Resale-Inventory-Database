653aad95da1d7926bbdd4a3b76898e2bb1f22b5b
Alex Ccls <alex.chapleau2345@gmail.com>
2024-04-23 11:16:43 -0500

    Added Comments

--- FinancialDatabase/InputSpreadsheetIntoDatabase.py
+++ FinancialDatabase/InputSpreadsheetIntoDatabase.py
@@ -4,8 +4,16 @@ import Pyscript.py
 A few cases
 	
 	- Bought Not Sold
+		- Item entry
+		- Purhcase entry
 	- Bought Not Sold, Packaged
-	- Bought     Sold, Packaged """
+		- Item entry
+		- Purchase entry
+		- Packaging Dims entry (Make this table)
+			- Item ID, weight(ttl oz, over-estimation), l,w,h
+	- Bought     Sold, Packaged
+		- Same as above
+		- Sold entry """
 
 # Given a row from input, determine if it has been
 	# 1 - Bought, Not Sold
