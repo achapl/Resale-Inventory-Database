6cc9c55a96cb20f8edf4c18bb10cab547d810fdd
Alex Ccls <alex.chapleau2345@gmail.com>
2025-01-09 08:16:39 -0600

    Added itemViewTab testing class

---
+++ FinancialDatabaseTesting/TestItemViewTab.cs
@@ -0,0 +1,39 @@
+﻿using FinancialDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialDatabaseTesting
+{
+    public class TestItemViewTab
+    {
+
+        public static Form1 form1;
+        public static TabController tabController;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetup()
+        {
+            form1 = new Form1();
+            tabController = new TabController(form1);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            form1.Dispose();
+        }
+
+
+
+        [Test]
+        public static void Test_ItemViewTab()
+        {
+            ItemViewTab tab = new ItemViewTab(tabController, form1);
+
+            tab.setCurrItem(Database.getItem())
+        }
+    }
+}
